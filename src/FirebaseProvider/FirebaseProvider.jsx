2bdc30c6b8983d1c95cfc4636e11bde0c9b182d5
maruf hossen <mdmdrufmeze@gmail.com>
2024-06-02 22:25:03 -0700

    add error code

--- src/FirebaseProvider/FirebaseProvider.jsx
+++ src/FirebaseProvider/FirebaseProvider.jsx
@@ -1,77 +1,77 @@
 
 
-// import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-// // import auth from "../firebase/firebase.config";
-// import { createContext, useEffect, useState } from 'react';
-// import { GoogleAuthProvider} from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+// import auth from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from 'react';
+import { GoogleAuthProvider} from 'firebase/auth';
 
 
 
-// export const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
-// // socal auth provider
-// const googleProvider = new GoogleAuthProvider();
+// socal auth provider
+const googleProvider = new GoogleAuthProvider();
 
 
 
-// const FirebaseProvider = ({children}) => {
-// const [user, setUser] = useState(null);
-// const [loading, setLoading] = useState(true);
+const FirebaseProvider = ({children}) => {
+const [user, setUser] = useState(null);
+const [loading, setLoading] = useState(true);
 
-// // create User
-// const createUser = (email, password) => {
-//     return createUserWithEmailAndPassword(auth, email, password)
-//  }
+// create User
+const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+ }
 
-// // Update user Profile
+// Update user Profile
 
-// const  updateUserProfile = (name , image)=>{
-//     return updateProfile(auth.currentUser, {
-//         displayName: name, photoURL: image
-//       })
-// }
+const  updateUserProfile = (name , image)=>{
+    return updateProfile(auth.currentUser, {
+        displayName: name, photoURL: image
+      })
+}
 
-// //  Sign In user
-// const signInUser = (email, password)=>{
-//    return signInWithEmailAndPassword(auth, email, password)
-// }
-// // Sign out
-// const logOut = ()=>{
-//     setUser(null)
-//     signOut(auth)
-// }
-// // google Login
-// const googleLogin = () =>{
-//  return signInWithPopup(auth, googleProvider)
-// }
+//  Sign In user
+const signInUser = (email, password)=>{
+   return signInWithEmailAndPassword(auth, email, password)
+}
+// Sign out
+const logOut = ()=>{
+    setUser(null)
+    signOut(auth)
+}
+// google Login
+const googleLogin = () =>{
+ return signInWithPopup(auth, googleProvider)
+}
 
  
-//  useEffect(()=>{
-//     onAuthStateChanged(auth, (user) => {
-//         if (user) {
-//           setUser(user)
-//         } 
-//       });
-//  },[])
+ useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(user)
+        } 
+      });
+ },[])
  
-//      const authInfo = {
-//           user,
-//           loading,
-//          createUser,
-//          signInUser,
-//          googleLogin,
-//          logOut,
-//          updateUserProfile
+     const authInfo = {
+          user,
+          loading,
+         createUser,
+         signInUser,
+         googleLogin,
+         logOut,
+         updateUserProfile
 
-//      }
-//     return (
-//         <AuthContext.Provider value={authInfo}>
-//          {children}
-//       </AuthContext.Provider>
-//     );
-// };
+     }
+    return (
+        <AuthContext.Provider value={authInfo}>
+         {children}
+      </AuthContext.Provider>
+    );
+};
 
-// export default FirebaseProvider;
-// // uthProvider.prototypes = {
-// //     children: PropTypes.node,
-// // }
+export default FirebaseProvider;
+// uthProvider.prototypes = {
+//     children: PropTypes.node,
+// }
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,65 +1,135 @@
 import { useForm } from "react-hook-form";
 
-
 const Registration = () => {
-  const {register, handleSubmit, watch, formState: {errors}} = useForm();
- const onSubmit = data => {
-  console.log(data)
-}
-    // const handelRegister = e =>{
-    //     e.preventDefault()
-    //     const form = e.target;
-    //     const name = form.name.value;
-    //     const email = form.email.value;
-    //     const photo = form.photo.value;
-    //     const password = form.password.value;
-    //     console.log(name, email, photo, password)
-    //   }
-    return (
-        <div>
-        <div className='flex justify-center items-center mt-10 mb-20'>
-         <div className="card shrink-0 w-[50%] shadow-2xl bg-base-100">
-         <form  onSubmit={handleSubmit(onSubmit)}  className="card-body">
-            <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input type="text" placeholder="Name" {...register("name", { required: true })} name="name" className="input input-bordered" required />
-                {errors.name && <span className="text-red-800 mt-2">This field is required</span>}
-
-              </div>
-              
-              <div className="form-control">
-             <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="email" {...register("email",{ required: true })} name="email" className="input input-bordered" required />
-                {errors.email && <span className="text-red-800 mt-2">This field is required</span>}
-
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Photo URL</span>
-                </label>
-                <input type="text" placeholder="PhotoURL" {...register("photo", { required: true })} name="photo" className="input input-bordered" required />
-                {errors.photo && <span className="text-red-800 mt-2">This field is required</span>}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" placeholder="password" {...register("password", { required: true,maxLength: 6, minLength:4, pattern: /^[A-Za-z]+$/i   })}  name="password" className="input input-bordered" required />
-                {errors.password?.type == 'required' && <span className="text-red-800 mt-2">This field is required</span>}
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
-       </div>
-       </div>
-      </div>
+  const {
+    register,
+    handleSubmit,
    
-    );
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  // const handelRegister = e =>{
+  //     e.preventDefault()
+  //     const form = e.target;
+  //     const name = form.name.value;
+  //     const email = form.email.value;
+  //     const photo = form.photo.value;
+  //     const password = form.password.value;
+  //     console.log(name, email, photo, password)
+  //   }
+  return (
+    <div>
+      <div className="flex justify-center items-center mt-10 mb-20">
+        <div className="card shrink-0 w-[50%] shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                {...register("name", { required: true })}
+                name="name"
+                className="input input-bordered"
+                required
+              />
+              {errors.name && (
+                <span className="text-red-800 mt-2">
+                  This field is required
+                </span>
+              )}
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                {...register("email", { required: true })}
+                name="email"
+                className="input input-bordered"
+                required
+              />
+              {errors.email && (
+                <span className="text-red-800 mt-2">
+                  This field is required
+                </span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                placeholder="PhotoURL"
+                {...register("photo", { required: true })}
+                name="photo"
+                className="input input-bordered"
+                required
+              />
+              {errors.photo && (
+                <span className="text-red-800 mt-2">
+                  This field is required
+                </span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                {...register("password", {
+                  required: true,
+                  maxLength: 6,
+                  minLength: 4,
+                  pattern: /^[A-Za-z]+$/i,
+                })}
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              {errors.password?.type == "required" && (
+                <span className="text-red-800 mt-2">
+                  This field is required
+                </span>
+              )}
+              {errors.password?.type == "maxLength" && (
+                <span className="text-red-800 mt-2">
+                  Password must be lassthen 6 characters
+                </span>
+              )}
+              {errors.password?.type == "minLength" && (
+                <span className="text-red-800  mt-2">
+                  Password must be 4 characters
+                </span>
+              )}
+              {errors.password?.type == "pattern" && (
+                <span className="text-red-800   mt-2">
+                  Password must be one uppercase one lowercase one number one
+                  special characters
+                </span>
+              )}
+            </div>
+            <div className="form-control mt-6">
+              <input
+                type="submit"
+                value="Sign Up"
+                className="btn  btn-primary"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
