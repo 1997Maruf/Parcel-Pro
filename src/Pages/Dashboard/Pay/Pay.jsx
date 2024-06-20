1717416f5d8593ba9a242c6bb6b7b5c35304954f
maruf hossen <mdmdrufmeze@gmail.com>
2024-06-20 09:55:23 -0700

    install stripe/stripe-js

--- src/Pages/Dashboard/Pay/Pay.jsx
+++ src/Pages/Dashboard/Pay/Pay.jsx
@@ -1,8 +1,14 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe(import.meta.env.VITE_Pay_Gateway_Pk);
 const Pay = () => {
     return (
         <div>
-            <h2>pay</h2>
+            <Elements stripe={stripePromise}>
+              <CheckoutForm></CheckoutForm>
+            </Elements>
         </div>
     );
 };
